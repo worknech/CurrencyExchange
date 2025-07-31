bab316696bbd71bb6f9ad2346a1ea1f22c8e9394
worknech <worknech@yandex.ru>
2025-07-31 18:21:06 +0300

    Добавлена функция exchange

--- exchenge.py
+++ exchenge.py
@@ -5,7 +5,21 @@ from tkinter import messagebox as mb
 
 
 def exchange():
-    pass
+    code = entry.get()
+    if code:
+        try:
+            response = requests.get('https://open.er-api.com/v6/latest/USD')
+            response.raise_for_status()
+            data = response.json()
+            if code in data['rates']:
+                exchange_rate = data['rates'][code]
+                mb.showinfo('Курс обмена', f'Курс: {exchange_rate:.2f} за 1 доллар')
+            else:
+                mb.showerror('Ошибка!', f'Валюта {code} не найдена')
+        except Exception as e:
+            mb.showerror('Ошибка', f'Произошла ошибка {e}.')
+    else:
+        mb.showwarning('Внимание!', 'Введите код валюты!')
 
 
 root = Tk()
