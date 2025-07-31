9fbbdc1c4d8b068c3e1b792efa8973e1fe5da02d
worknech <worknech@yandex.ru>
2025-07-31 18:52:52 +0300

    Вместо поля ввода добавлен выпадающий список

--- exchenge.py
+++ exchenge.py
@@ -1,11 +1,11 @@
 import requests
 import json
 from tkinter import *
-from tkinter import messagebox as mb
+from tkinter import messagebox as mb, ttk
 
 
 def exchange():
-    code = entry.get()
+    code = combobox.get()
     if code:
         try:
             response = requests.get('https://open.er-api.com/v6/latest/USD')
@@ -26,10 +26,14 @@ root = Tk()
 root.title('Кусры обмена валют')
 root.geometry('500x500')
 
-Label(root, text='ВВедите код валюты').pack(padx=10, pady=10)
+Label(root, text='Выберите код валюты').pack(padx=10, pady=10)
 
-entry = Entry(root)
-entry.pack(padx=10, pady=10)
+cur = ['RUB', 'EUR', 'GBP', 'CNY', 'KXT', 'UZS', 'CHF', 'AED', 'CAD']
+combobox = ttk.Combobox(root, values=cur)
+combobox.pack(padx=10, pady=10)
+
+# entry = Entry(root)
+# entry.pack(padx=10, pady=10)
 
 Button(text='Получить курс обмена к доллару', command=exchange).pack(padx=10, pady=10)
 
